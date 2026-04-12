2ec298a54113a598a913d16fc8aeee1d2a329122
Jonathan Tran <jonathan@JT-FROYO.localdomain>
2026-04-12 11:18:57 -0500

    Add OLED timeout

--- keyboards/crkbd/rev1/keymaps/j0natran/config.h
+++ keyboards/crkbd/rev1/keymaps/j0natran/config.h
@@ -41,3 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
 #endif
+
+#ifdef OLED_ENABLE
+    #define OLED_TIMEOUT 60000
+#endif
